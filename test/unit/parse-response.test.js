4a026da8b9e5b7b36fdc628441a96a93416f7271
Jarrod Linahan <jarrod@linahan.id.au>
2019-05-19 18:04:38 +1000

    Response parser test

--- test/unit/parse-response.test.js
+++ test/unit/parse-response.test.js
@@ -24,4 +24,33 @@ describe('XML Response Parser', () => {
     expect(data.code).toBe(300)
     expect(data.detail).toBe('success')
   })
+
+  it('should throw an error if given invalid XML', () => {
+    expect(() => {
+      let data = parseResponse('{this: "is not xml"}')
+      console.log('PARSED RESOPONSE!?!?!??!?!', data)
+    }).toThrow()
+  })
+
+  it('should throw an error if no namesilo reply exists', () => {
+    expect(() => {
+      let data = parseResponse('<namesilo></namesilo>')
+      console.log('PARSED RESOPONSE!?!?!??!?!', data)
+    }).toThrow()
+  })
+
+  it('should flatten "hosts" array (listRegisteredNameServers)', async () => {
+    let xml = await loadFixture('listRegisteredNameServers')
+    let data = parseResponse(xml)
+
+    expect(utils.isObject(data)).toBe(true)
+    expect(data.code).toBe(300)
+    expect(data.detail).toBe('success')
+    expect(utils.isArray(data.hosts)).toBe(true)
+
+    let ex = data.hosts[0]
+    expect(utils.isString(ex.host)).toBe(true)
+    expect(utils.isArray(ex.ip)).toBe(true)
+    expect(utils.isString(ex.ip[0])).toBe(true)
+  })
 })
